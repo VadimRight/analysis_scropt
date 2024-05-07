81d9d477fe4e0ed11f93009ae8e69682efe6da84
VadimRight <vadimright@mail.ru>
2024-05-07 11:28:50 +0300

    Post create and delete requests are app

--- endpoints_execution.py
+++ endpoints_execution.py
@@ -1,36 +1,41 @@
 import requests
-import time
 import json
+import os
 
-# Load user data
+
 with open("./jsons/test_user.json") as test_user_file:
     test_user_data = json.load(test_user_file)
 
-# Set the base URL
 url = "http://localhost:8000"
+file_path='./Screen.png'
 
-# Register user
+
 user_registration = requests.post(f"{url}/register", json=test_user_data)
-time.sleep(0.5)  # Wait for half a second to ensure the registration completes
 
-# Get the token
+
 token_response = requests.post(
     f"{url}/token", 
     data={"username": "test", "password": "test", "grant_type": "password"}, 
     headers={"content-type": "application/x-www-form-urlencoded"}
 )
 token_json = token_response.json()
-token = token_json["access_token"]  # Extract the access token
+token = token_json["access_token"]  
 
-# Use the token in subsequent requests
+
 headers = {"Authorization": f"Bearer {token}"}
-profile_response = requests.get(f"{url}/profile", headers=headers)
-delete_user_response = requests.delete(f"{url}/profile/delete_user", json={"username": "test"}, headers=headers)
 
-# Print responses for debugging
-print("Registration Response:", user_registration.status_code)
-print("Token Response:", token_response.status_code)
-print("Token:", token)
-print("Profile Response:", profile_response.status_code)
-print("Delete User Response:", delete_user_response.status_code)
 
+requests.get(f"{url}/profile", headers=headers)
+
+
+f = open(file_path, 'rb')
+
+
+create_post = requests.post(f"{url}/new_post", headers=headers, params={'text': 'test', 'name': 'test'}, files={'image': (os.path.basename(file_path), f, 'multipart/form-data')}).json()
+post_id=create_post[0]["id"]
+
+requests.delete(f"{url}/profile/posts={post_id}", headers=headers, params={"id":f"{post_id}"})
+requests.delete(f"{url}/profile/delete_user", json={"username": "test"}, headers=headers)
+
+
+f.close()
